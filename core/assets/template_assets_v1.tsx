ecf60bb081ec62bc7bf3011f7ba257b92e178845
DESKTOP-UL9PSVK\micro <mercurial@fantastic.me.uk>
2018-01-27 15:29:03 +0000

    apply some tags to tile in tileset

--- core/assets/template_assets_v1.tsx
+++ core/assets/template_assets_v1.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="template_assets_v1" tilewidth="128" tileheight="128" tilecount="120" columns="15">
  <image source="template_assets_v1.png" width="1920" height="1080"/>
+ <tile id="36">
+  <properties>
+   <property name="origin" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="goal" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="mirror" type="int" value="90"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="mirror" type="int" value="45"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="mirror" type="int" value="135"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="mirror" type="int" value="0"/>
+  </properties>
+ </tile>
 </tileset>
