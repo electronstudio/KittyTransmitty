a7c7cd29d31453f4722a29d03a41e8c8a55ede16
DESKTOP-UL9PSVK\micro <mercurial@fantastic.me.uk>
2018-01-27 16:31:12 +0000

    title screen

--- core/assets/template_assets_v1.tsx
+++ core/assets/template_assets_v1.tsx
@@ -1,14 +1,59 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="template_assets_v1" tilewidth="128" tileheight="128" tilecount="120" columns="15">
  <image source="template_assets_v1.png" width="1920" height="1080"/>
+ <tile id="18">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="36">
   <properties>
-   <property name="origin" type="bool" value="true"/>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="51">
   <properties>
-   <property name="goal" type="bool" value="true"/>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="54">
@@ -29,6 +74,36 @@
  <tile id="57">
   <properties>
    <property name="mirror" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="obstacle" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="origin" type="bool" value="true"/>
   </properties>
  </tile>
 </tileset>
