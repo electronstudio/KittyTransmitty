1c78dc69365fbb7d343a1b6f5e2192729e2feff7
Leonario Fernandes <Leonario@Leonarios-MacBook-Pro.local>
2018-01-27 14:49:50 +0000

    first

---
+++ core/assets/template_assets_v1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="template_assets_v1" tilewidth="128" tileheight="128" tilecount="120" columns="15">
+ <image source="template_assets_v1.png" width="1920" height="1080"/>
+</tileset>
